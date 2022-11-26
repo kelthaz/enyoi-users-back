c74caea21aa832cdb447d5ba6b9fa5ebcb0f7168
Kelthaz <davidkelthaz@gmail.com>
2022-11-25 19:27:54 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,148 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+
+const conexionBD = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'ventas'
+})
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+const  obtenerUsuario = () => {
+    return {
+        id : 1,
+        nombre : 'Carlos',
+        usuario : 'C2',
+        estado : true
+    }
+}
+
+app.use((req, res, next) => {
+    const usuarioPermitido = obtenerUsuario() 
+    req.locals = {
+        usuarioPermitido
+    }
+    next()
+})
+
+const middlwareAgregarUsuario = (req, res, next) => {
+    console.log(' Se ejecuto el middlwareAgregarUsuario')
+    const usuarioPermitido = obtenerUsuario() 
+    //middleware para agregar usuario
+    if (usuarioPermitido.usuario === "Cd" ){
+        req.locals = {
+            usuarioPermitido,
+        }
+        next()
+    } else {
+        req.locals = 'Usuario sin permisos'
+        console.log('Usuario no permitido')
+        next()
+    }
+}
+
+app.get('/', (req,res) => {
+    res.send('Bienvenidos al inicio')
+})
+
+//Api para traer todos los usuarios 
+app.get('/usuarios', (req, res) => {
+    const sql = 'SELECT * FROM usuarios'
+
+    conexionBD.query(sql, (error, results) => {
+      if (error) throw error
+
+      if (results.length > 0) {
+        res.json(results)
+      } else {
+        res.send('No hay datos disponibles')
+      }
+    })
+    // res.send('Esta es la lista de usuarios')
+})
+
+// Api para traer solo un usuario por ID 
+app.get('/usuario/:idusuario', (req, res) => {
+    const id = req.params
+    const sql = `SELECT * FROM usuarios WHERE  idusuarios = ${id.idusuario}`
+
+    conexionBD.query(sql, (error, result) => {
+        if (error) throw error
+
+        if(result.length > 0){
+          res.json(result)
+        } else {
+          res.send('Usuario no encontrado')
+        }
+    })
+})
+
+
+// Api para crear usuarios
+app.post('/agregar-usuario',(req,res) => {
+    const sql = 'INSERT INTO usuarios SET ?'
+
+    const usuarioObj = {
+      idusuarios : req.body.idusuario,
+      nombre : req.body.nombre,
+      usuario : req.body.usuario,
+    }
+
+    conexionBD.query(sql, usuarioObj, error => {
+      if (error) throw error
+
+      res.send('Usuario creado con exito!')
+    })
+})
+
+//Api para actualizar al usuario
+app.put('/actualizar-usuario/:usuarioId',(req, res) => {
+    const id = req.params
+    const {nombre,usuario} = req.body 
+
+    const sql = `UPDATE usuarios SET nombre = '${nombre}', usuario = '${usuario}' where idusuarios = ${id.usuarioId}`
+
+    conexionBD.query(sql, error => {
+      if (error) throw error
+
+      res.send('Usuario actualizado con exito!')
+    })
+})
+
+//Api para eliminar un usuario
+app.delete('/eliminar-usuario/:usuarioId', (req, res) => {
+    const id = req.params
+    const sql = `DELETE FROM usuarios where idusuarios = ${id.usuarioId}`
+
+    conexionBD.query(sql, error => {
+      if (error) throw error
+
+      res.send('Usuario eliminado con exito!')
+    })
+})
+
+//El puerto esta activo
+app.listen(3001, () => {
+    console.log('Servidor en el puerto 3001')
+})
+
+// CRUD
+
+// c =  CREATE
+// R = READ
+// U = UPDATE
+// D = DELETE 
