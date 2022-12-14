4572f581cb354f6a0f9bca86f4d3ffa59443b59f
Kelthaz <davidkelthaz@gmail.com>
2022-12-14 17:05:20 -0500

    eliminacion required

--- src/app.js
+++ src/app.js
@@ -1,16 +1,6 @@
 import { pool } from './db.js'
-const express = require('express')
-const mysql = require('mysql')
 
 const app = express()
-
-const conexionBD = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT  
-})
 
 
 app.use(express.json())
