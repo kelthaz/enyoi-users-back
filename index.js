4d061f7dbda90b54e4784c2babbcc3ce810704c1
Kelthaz <davidkelthaz@gmail.com>
2022-12-14 14:24:32 -0500

    env

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+
+const config = require('dotenv')
 const express = require('express')
 const mysql = require('mysql')
 
 const app = express()
 
 const conexionBD = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'ventas'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 
